fd0349589dac6d271d53e32899d136151236c1ec
Lepaksh Gujar <lepakshgujar142@gmail.com>
2024-11-07 09:26:10 +0530

    second commit

---
+++ november6/function.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void add(int a, int b){
+    printf("%d\n",a+b);
+}//function definition
+int main(){
+    add(5,6);//function call
+    add(10,20);
+    add(5,10);
+}
---
+++ november6/functionadd.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int add(){
+    int a=6, b=7;
+    return a+b;
+}
+int main(){
+    printf("%d",add());
+}
